270befdd2bdbd2df8ad9428d1fee123b8b6dcbdc
Robert Luiz <robertlsc2016@gmail.com>
2023-11-28 13:41:37 -0300

    position class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ChessProject
+﻿using board;
+
+namespace ChessProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Position P;
+            P = new Position(3, 4);
+
+            Console.WriteLine("Position" + P);
         }
     }
 }
